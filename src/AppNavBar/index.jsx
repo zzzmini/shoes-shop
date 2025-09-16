3d3b0b8aea64dde4ec4708135eab71baeae18377
zzzmini <sm01079200887@gmail.com>
2025-09-16 16:40:31 +0900

    react-router-dom 설치 및 셋팅

--- src/AppNavBar/index.jsx
+++ src/AppNavBar/index.jsx
@@ -1,6 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import './AppNavBar.css'
+// Route : 페이지(컴포넌트) 이동 처리
+// Routes : url에 담겨있는 정보를 획득
+// Link : 실제로 페이지를 보여주는 역할, Link위치에 컴포넌트 뿌려줌
+import { Route, Routes, Link } from "react-router-dom";
 
 function AppNavBar(){
   return (
@@ -8,14 +12,28 @@ function AppNavBar(){
       <div className="App">
         <Navbar bg="dark" data-bs-theme="dark">
           <Container>
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Navbar.Brand>Muzinjang</Navbar.Brand>
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <Nav.Link>
+                <Link to={"/"}>Home</Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to={"/detail"}>Detail</Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to={"/cart"}>Cart</Link>
+              </Nav.Link>
             </Nav>
           </Container>
         </Navbar>
+
+        {/* Routing 정보를 한꺼번에 모아놓는 장소 */}
+        {/* 스프링에서 사용하는 컨트롤러 클래스 */}
+        <Routes>
+          <Route path="/" element={<div>메인페이지</div>} />
+          <Route path="/detail" element={<div>상세페이지</div>} />
+          <Route path="/cart" element={<div>장바구니페이지</div>} />
+        </Routes>
       </div>
     </>
   )
