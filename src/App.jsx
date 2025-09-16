bddcf4a957e406a6cf038e7bfba697dc22133e95
zzzmini <sm01079200887@gmail.com>
2025-09-16 15:51:00 +0900

    product 콤포넌트 작업 완료

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ import img_2 from "./assets/images/shoes2.jpg"
 import {Container, Row, Col} from 'react-bootstrap';
 import data from "./data/data";
 import { useState } from "react";
+import Product from "./Product";
 
 function App() {
   // 상품정보를 갖는 product 스테이트를 만든다.
   const [product, setProduct] = useState(data);
-  console.log(product)
 
   return (
     <>
@@ -28,24 +28,16 @@ function App() {
       {/* 상품진열영역 끝 */}
       <Container>
         <Row>
-          <Col className="text-center">
-            <img src="https://zzzmini.github.io/images/shoes1.jpg" 
-            width="80%" />
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
-          <Col>
-            <img src={img_2}
-            width="80%" />
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
-          <Col>
-            <img src="/images/shoes3.jpg" 
-            width="80%" />
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
+          {
+            product.map((shoes, _)=>{
+              return(
+                <Col key={shoes.id} className="text-center">
+                  {/* Product 콤포넌트 자리 */}
+                  <Product shoes={shoes}/>
+                </Col>
+              )
+            })
+          }
         </Row>
       </Container>
     </>
