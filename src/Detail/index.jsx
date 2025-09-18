f773a9f8bdbb42ec2c0cfe7a5ad370b958d2bedf
zzzmini <sm01079200887@gmail.com>
2025-09-18 15:33:02 +0900

    useEffect 처리

--- src/Detail/index.jsx
+++ src/Detail/index.jsx
@@ -1,6 +1,38 @@
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import Discount from "../Discount";
 
 function Detail({product}){
+
+  const [showAlert, setShowAlert] = useState(true)
+  const [inputData, setInputData] = useState('')
+  // 숫자말고 문자 입력 시 처리를 확인 할 논리값
+  const [state, setState] = useState(false)
+
+  // useEffect 실행 확인
+  useEffect(()=>{
+    // 타이머를 붙이고 2초 후에 Discount가 사라지도록
+    const myTimer = setTimeout(()=> setShowAlert(false), 2000);
+    // 기존 사용한 타이머를 삭제
+    return ()=>{
+      clearTimeout(myTimer);
+    }
+    // 처음 실행 될 때 딱 한번만...
+  }, [])
+
+  // 입력 수량 확인 용 Effect
+  // input 상자에만 반응
+  useEffect(()=>{
+    // inputData state가 문자면...
+    // isNaN : is Not a Number
+    if(isNaN(inputData)){
+      setState(true)
+    } else {
+      setState(false)
+    }
+  }, [inputData])
+
+
   // detail/3 -> pathvariable 값을 확인...
   // hook : useParams
   // 파라미터를 변수로 저장할 때는 중괄로 사용 필수
@@ -24,6 +56,9 @@ function Detail({product}){
 
   return(
     <div className="container">
+        <div className="container mt-2">
+          {showAlert && <Discount />}
+        </div>
       <div className="row">
         <div className="col-md-6">
           <img src={`/images/shoes${findProduct.id+1}.jpg`} 
@@ -32,6 +67,12 @@ function Detail({product}){
         <div className="col-md-6">
           <h4 className="pt-5">{findProduct.title}</h4>
           <p>{findProduct.content}</p>
+          {/* 문자가 들어올 때 출력할 내용 */}
+          {state && <div>오류</div>}
+          <p>수량 : 
+            <input type="text" 
+              onChange={(e)=>{setInputData(e.target.value)}}/>
+          </p>
           <p>{findProduct.price}</p>
           <button className="btn btn-danger">주문하기</button>
         </div>
---
+++ src/Discount/index.jsx
@@ -0,0 +1,8 @@
+function Discount(){
+  return(
+    <div className="alert alert-waring">
+      2초 이내 구매시 할인
+    </div>
+  )
+}
+export default Discount;
